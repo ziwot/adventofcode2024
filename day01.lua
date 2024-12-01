c039e8580da4370c34872108b05a6cc6d3c13f88
steven j <sjehann@gmail.com>
2024-12-01 21:59:52 +0100

    day01 pt 2

--- day01.lua
+++ day01.lua
@@ -40,4 +40,19 @@ for i, v in ipairs(left) do
 	distance_sum = distance_sum + math.abs(v - right[i])
 end
 
+local similarity_score = 0
+
+for _, vl in ipairs(left) do
+	local count = 0
+	for _, vr in ipairs(right) do
+		if vl == vr then
+			count = count + 1
+		end
+	end
+	if count > 0 then
+		similarity_score = similarity_score + (vl * count)
+	end
+end
+
 print(distance_sum)
+print(similarity_score)
