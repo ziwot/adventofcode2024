b8dbbf5aa3322f91704252f8a4d113f263eeada2
steven j <sjehann@gmail.com>
2024-12-02 07:38:48 +0100

    wip day02

--- day02.lua
+++ day02.lua
@@ -3,4 +3,16 @@ local utils = require("utils")
 
 local input = utils.read_file("./day02.txt")
 
-print(inspect(input))
+local safe = {}
+
+for _, v in ipairs(input) do
+	local prev = nil
+	for _, vv in ipairs(v) do
+		if prev ~= nil then
+			print(prev, vv)
+		end
+		prev = vv
+	end
+end
+
+-- print(inspect(input))
