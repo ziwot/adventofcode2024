b7d01c9a0489e6f53e502f07f1d3e6997d10f37d
steven j <sjehann@gmail.com>
2024-12-03 07:40:17 +0100

    day1 pt 2 wip

--- day02.lua
+++ day02.lua
@@ -3,17 +3,19 @@ local utils = require("utils")
 
 local input = utils.read_file("./day02.txt")
 
-local safeReportFilter = function(v)
+badLevels = function(v)
 	local prev = nil
 	local order = nil
 	local currOrder = nil
+	local indexes = {}
 
-	for _, vv in ipairs(v) do
+	for i, vv in ipairs(v) do
 		local n = tonumber(vv)
 
 		if prev ~= nil then
 			if prev == n then
-				return false
+				table.insert(indexes, i)
+				table.insert(indexes, i)
 			elseif prev < n then
 				currOrder = ">"
 			else
@@ -25,22 +27,60 @@ local safeReportFilter = function(v)
 			end
 
 			if order ~= currOrder then
-				return false
+				table.insert(indexes, i)
 			end
 
 			local distance = math.abs(prev - n)
 
 			if distance < 1 or distance > 3 then
-				return false
+				table.insert(indexes, i)
 			end
 		end
 
 		prev = n
 	end
 
+	return indexes
+end
+
+local safeReportFilterPt1 = function(v)
+	local bl = badLevels(v)
+
+	if #bl > 0 then
+		return false
+	end
+
+	return true
+end
+
+-- fixme
+local safeReportFilterPt2 = function(v)
+	local bl = badLevels(v)
+
+	if #bl > 0 then
+		for _, i in ipairs(bl) do
+			local vv = {}
+			for j, vvv in ipairs(v) do
+				if i ~= j then
+					table.insert(vv, vvv)
+				end
+			end
+
+			local stillBad = badLevels(vv)
+			if #stillBad == 0 then
+				return true
+			else
+				print(inspect(stillBad))
+			end
+		end
+
+		return false
+	end
+
 	return true
 end
 
 -- pt 1.
-local safe = utils.filter_inplace(input, safeReportFilter)
+-- local safe = utils.filter_inplace(input, safeReportFilterPt1)
+local safe = utils.filter_inplace(input, safeReportFilterPt2)
 print(#safe)
--- utils.lua
+++ utils.lua
@@ -32,12 +32,4 @@ M.filter_inplace = function(t, filterIter)
 	return out
 end
 
-M.tablelength = function(T)
-	local count = 0
-	for _ in pairs(T) do
-		count = count + 1
-	end
-	return count
-end
-
 return M
